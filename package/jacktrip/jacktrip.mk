898e5ddc538f2c99415a679ef2ce162bd0a8d7e7
Marcus Weseloh <marcus@weseloh.cc>
2024-04-23 14:17:25 +0200

    Upgrade to jacktrip 2.2.5

--- package/jacktrip/jacktrip.mk
+++ package/jacktrip/jacktrip.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-JACKTRIP_VERSION = 1.7.0
+JACKTRIP_VERSION = 2.2.5
 JACKTRIP_SOURCE = jacktrip-$(JACKTRIP_VERSION).tar.gz
 JACKTRIP_SITE = $(call github,jacktrip,jacktrip,v$(JACKTRIP_VERSION))
 JACKTRIP_LICENSE = GPL-3.0+
 JACKTRIP_LICENSE_FILES = LICENSE.md
 JACKTRIP_INSTALL_STAGING = NO
 
+JACKTRIP_MESON_EXTRA_BINARIES += moc='$(HOST_DIR)/libexec/moc'
 JACKTRIP_CONF_OPTS += -Dnogui=true -Djack=enabled -Drtaudio=disabled
 
 $(eval $(meson-package))
