73ae4fa26839f29694b0fcf7081deb0e0b1ca8ab
Marcus Weseloh <marcus@weseloh.cc>
2024-07-09 17:05:32 +0200

    Use all SNAPPI* env vars in build script

--- build/run.sh
+++ build/run.sh
@@ -3,11 +3,18 @@
 ALL_ARGS=("$@")
 COMPONENT="$1"
 
+# forward all SNAPPI* env vars to container
+ENV_OPTS=""
+for var in $(env | grep ^SNAPPI); do
+    ENV_OPTS="${ENV_OPTS} -e ${var}"
+done
+
 podman run -it --rm \
     -v ./build/:/build \
     -v ./snappi:/snappi \
     -v ./dl:/buildroot/dl \
     -w /build/$COMPONENT \
+    ${ENV_OPTS} \
     --userns="keep-id:uid=1001,gid=1001" \
     buildroot \
     ${ALL_ARGS[@]:1}
