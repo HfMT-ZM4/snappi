5178e551e833b0ce0a7368a22be216253db17faa
Marcus Weseloh <marcus@weseloh.cc>
2024-07-16 14:32:41 +0200

    Allow suffix for release files

--- build/prepare_release.sh
+++ build/prepare_release.sh
@@ -3,11 +3,12 @@
 RELEASE_DIR=$1
 BUILD_DIR=$2
 VERSION=$3
+ADDITION=${4}
 
 COMPONENT=`grep BR2_EXTERNAL_NAMES ${BUILD_DIR}/.config | cut -d \" -f2 | tr '[:upper:]' '[:lower:]'`
 VARIANT=`ls -1 ${BUILD_DIR}/images/*.dtb | xargs basename -s .dtb`
 
-NAME=${COMPONENT}-${VERSION}-${VARIANT}
+NAME=${COMPONENT}-${VERSION}-${VARIANT}${ADDITION}
 IMAGE_NAME=${NAME}.img
 
 echo Copying ${BUILD_DIR}/images/sdcard.img to ${RELEASE_DIR}/${IMAGE_NAME}
